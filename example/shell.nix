9a3c9995647eed6c46bb32320c9a7e75a08dacba
marcesquerra <esquerra@gmail.com>
2021-04-24 18:10:23 +0000

    [skip actions] Update example

--- example/shell.nix
+++ example/shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  tfnix = import (fetchTarball "https://github.com/bryght-space/tfnix/archive/de6a229f1a9099d0f3d2453a4e3096a725b7bb07.tar.gz");
+  tfnix = import (fetchTarball "https://github.com/bryght-space/tfnix/archive/b81518400dc44c599e129b390c22dc7576571f41.tar.gz");
   terraform = tfnix ./.;
 in
   pkgs.mkShell {
